0773de2b64d1452ea85fb5cd0fd0645a7da1fc4a
CarlosErrera <su1rgg@mail.com>
2018-04-16 15:29:58 +0300

    added dropdown menu in myclients

---
+++ components/myclients/myclients.js
@@ -0,0 +1,27 @@
+export default{
+    data(){
+        return{
+            selected:'Выбрать',
+            isMenu:false,
+            clients:[
+                {
+                    id:1,
+                    date:'Только что',
+                    tel:'1234567890'
+                    //price:20000
+                }
+            ]
+        }
+    },
+    methods:{
+        showMenu(){
+            if(this.isMenu){
+                return this.isMenu = false;
+            }else{
+                return this.isMenu = true;
+            }             
+        }
+    }
+
+
+}
